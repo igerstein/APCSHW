34addbce3c1f9ac6ff05e431a5349fe21985f783
igerstein <isaac.gerstein1@gmail.com>
2014-12-16 19:10:35 -0500

    Added insertion sort and selection sort and made them take an array of integers

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,8 +1,14 @@
 import java.util.*;
 public class Sorts{
     public static void main(String[]args){
+	int[] a = {3, 1, 4, 0, 2, 5};
+	int[] b = {3, 1, 4, 0, 2, 5};
 	int[] c = {3, 1, 4, 0, 2, 5};
-	Sorts.bubble(c);
+	Sorts.bubble(a);
+	Sorts.insert(b);
+	Sorts.select(c);
+	System.out.println(Arrays.toString(a));
+	System.out.println(Arrays.toString(b));
 	System.out.println(Arrays.toString(c));
     }
     public static int[] bubble(int[]c){
@@ -14,6 +20,35 @@ public class Sorts{
 		    c[j + 1] = a;
 		}
 	    }
+	}
+	return c;
+    }
+    public static int[] insert(int[]c){
+	for (int i = 1; i < c.length; i++){
+	    int current = c[i];
+	    int j = 0;
+	    while (j < i && current > c[j]){
+		j++;
+	    }
+	    for (int k = i; k > j; k--){
+		c[k] = c[k - 1];
+	    }
+	    c[j] = current;
+	}
+	return c;
+    }	
+    public static int[] select(int[]c){
+	for (int i = 0; i < c.length; i++){
+	    int lowest = c[i];
+	    int swapIndex = i;
+	    for (int j = i; j < c.length; j++){
+		if (c[j] < lowest){
+		    lowest = c[j];
+		    swapIndex = j;
+		}
+	    }
+	    c[swapIndex] = c[i];
+	    c[i] = lowest;
 	}
 	return c;
     }
